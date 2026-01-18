8c8d34f53d663e0843a7a3db0f2d18c2b0c3577e
Ahmedmagdy790 <AhmedMagdy79019@gmail.com>
2026-01-19 01:41:10 +0200

    add hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}/>
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 )
